110825fb8cee173eba6d33741a3fa8d8c5236bcb
Rajashree0316 <rajashreeasok16@gmail.com>
2024-05-01 21:34:25 +0530

    media queries

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -18,7 +18,7 @@ const SearchForm = () => {
     <section className="section search">
       <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-control">
-          <label htmlFor="name">Search your favorite cocktail </label>
+          <label htmlFor="name">Search cocktail </label>
           <input type="text" id="name" ref={searchValue} onChange={searchCocktail}/>
         </div>
         </form>
